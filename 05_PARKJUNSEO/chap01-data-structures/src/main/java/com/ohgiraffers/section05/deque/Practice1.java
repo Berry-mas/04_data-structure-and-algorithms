219efe4639289983062a27eaeede7d703dcab5fe
junseo <junseo9145@gmail.com>
2025-04-14 17:49:01 +0900

    수업 종료 커밋

---
+++ 05_PARKJUNSEO/chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/Practice1.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section05.deque;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/* 풍선 터뜨리기 - 백준 (2346) */
+public class Practice1 {
+
+    static class Balloon{
+        int order;
+        int noteValue;
+
+        public Balloon(int order, int noteValue) {
+            this.order = order;
+            this.noteValue = noteValue;
+        }
+    }
+
+// =================================================================
+
+    public String solution(Integer[] notes) {
+        Deque<Integer> deque = new LinkedList<>();
+        
+
+        return null;
+    }
+}
