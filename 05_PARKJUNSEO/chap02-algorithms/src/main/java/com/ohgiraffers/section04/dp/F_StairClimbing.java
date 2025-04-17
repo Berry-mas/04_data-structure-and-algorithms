ea19cf99cc3b1e4a13841ddf840ce82736a7b93c
junseo <junseo9145@gmail.com>
2025-04-17 17:51:21 +0900

    20250417 수업 종료 커밋

--- 05_PARKJUNSEO/chap02-algorithms/src/main/java/com/ohgiraffers/section04/dp/F_StairClimbing.java
+++ 05_PARKJUNSEO/chap02-algorithms/src/main/java/com/ohgiraffers/section04/dp/F_StairClimbing.java
@@ -17,10 +17,26 @@ public class F_StairClimbing {
 
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-
         }
 
+        // 초기값 설정
+        dp[1] = arr[1];
+        if (n >= 2) dp[2] = dp[1] + arr[2];
 
-        return 0;           
+        // 점화식은 3번 계단부터 적용
+        for (int i = 3; i <= n; i++) {
+
+            /*
+            * 가능 상황 1. 직전 계단에서 올라오는 상황
+            * => 연달아 밟지 않아야 하므로 | i-3 -> i-1 -> i
+            *
+            * 가능 상황 2. 두 계단 전에 올라오는 상황 -> ㅑ-2 전의 상황을 고려할 필요 x
+            * 1번 : dp[i-3] + arr[i-1] + arr[i]
+            * 2번 : dp[i-2] + arr[i]
+            * */
+
+            dp[i] = Math.max(dp[i-3] + arr[i-1], dp[i-2]) + arr[i];
+        }
+        return dp[n];
     }
 }
